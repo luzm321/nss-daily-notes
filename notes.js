f6dd00a505e6d15c2982a7e7068f4fef53729642
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-05-27 11:09:09 -0500

    my first version of daily-notes of some things that I have learned from NSS thus far

---
+++ notes.js
@@ -0,0 +1,18 @@
+const notes = [
+    {
+        id: 1,
+        subject: "Git Commands and Version Control with Github",
+        date: "05/21/21",
+        feeling: "Initially, learning the git commands and version control with fixing merge conflicts were overwhelming, but after iterating some more, it felt more manageable.",
+        timeSpent: 180
+    },
+    {
+        id: 2,
+        subject: "CSS Flex-box",
+        date: "05/20/21",
+        feeling: "Learning how to manipulate the CSS Flex-box layout was challenging at first, but practicing with the group projects helped make it more understandable.",
+        timeSpent: 60
+    }
+];
+
+console.log(notes);
